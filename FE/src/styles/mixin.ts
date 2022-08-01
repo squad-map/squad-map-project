d5d3fe8c42f0f721c4f619d2bb07e78b5065bf29
muffin9 <jinlog9@gmail.com>
2022-08-01 21:07:59 +0800

    styles: 재사용될 속성들 만들기

--- FE/src/styles/mixin.ts
+++ FE/src/styles/mixin.ts
@@ -1,3 +1,5 @@
+import theme from './theme';
+
 interface Flexbox {
   dir: 'row' | 'column';
   jc: string;
@@ -9,4 +11,38 @@ export const flexbox = ({ dir = 'row', jc, ai }: Partial<Flexbox>) => `
   flex-direction: ${dir};
   justify-content: ${jc};
   align-items: ${ai};
+`;
+
+export const inlineFlexbox = ({ dir = 'row', jc, ai }: Partial<Flexbox>) => `
+display: inline-flex;
+flex-direction: ${dir};
+justify-content: ${jc};
+align-items: ${ai};
+`;
+
+/* typography */
+type FontWeight = 400 | 700;
+export const typoDisplay = (fontWeight: FontWeight) => `
+  font-size: ${theme.fontSize.display};
+  font-weight: ${fontWeight};
+`;
+
+export const typoLarge = (fontWeight: FontWeight) => `
+  font-size: ${theme.fontSize.lg};
+  font-weight: ${fontWeight};
+`;
+
+export const typoMedium = (fontWeight: FontWeight) => `
+  font-size: ${theme.fontSize.md};
+  font-weight: ${fontWeight};
+`;
+
+export const typoSmall = (fontWeight: FontWeight) => `
+  font-size: ${theme.fontSize.sm};
+  font-weight: ${fontWeight};
+`;
+
+export const typoXSmall = (fontWeight: FontWeight) => `
+  font-size: ${theme.fontSize.xs};
+  font-weight: ${fontWeight};
 `;
