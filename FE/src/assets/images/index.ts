21441667f427e573061c0e401cddc8d7c98f5ee9
Muffin <jinlog9@gmail.com>
2022-08-13 12:20:23 +0900

    feat: interface 분리작업, image 파일 추가

--- FE/src/assets/images/index.ts
+++ FE/src/assets/images/index.ts
@@ -1,7 +1,9 @@
+import LoginBackground from '@/assets/images/loginBackground.svg';
 import Logo from '@/assets/images/logo.svg';
 import Menu from '@/assets/images/menu.svg';
 
 export const Images = {
+  LoginBackground,
   Logo,
   Menu,
 } as const;
