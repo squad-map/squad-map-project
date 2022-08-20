bd507c816916e668b3a3e16aadd86648b8d14a3c
muffin9 <jinlog9@gmail.com>
2022-08-20 14:57:48 +0800

    refactor: Home Page story msw 연동, Card에 링크 해당 맵에 대한 달기

--- FE/src/pages/Home/Home.stories.tsx
+++ FE/src/pages/Home/Home.stories.tsx
@@ -1,10 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { rest } from 'msw';
+
 import HomePage from './index';
+
+import { homeMapsData } from '@/constants/dummyData';
 
 export default {
   title: 'HomePage',
   component: HomePage,
-};
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get('/', (req, res, ctx) =>
+          res(ctx.status(200), ctx.delay(1000), ctx.json(homeMapsData))
+        ),
+      ],
+    },
+  },
+} as ComponentMeta<typeof HomePage>;
 
-const Template = () => <HomePage />;
+const Template: ComponentStory<typeof HomePage> = () => <HomePage />;
 
 export const Default = Template.bind({});
