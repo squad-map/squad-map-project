cddd03071c5ddc5e8737175bd9884048df47e5fe
Minseok-Choi <cms05041@gmail.com>
2022-08-24 16:33:18 +0900

    chore: rest docs 적용 실습

---
+++ BE/src/test/java/com/squadmap/acceptance/map/MapTest.java
@@ -0,0 +1,6 @@
+package com.squadmap.acceptance.map;
+
+import com.squadmap.assured.RestAssuredTest;
+
+public class MapTest extends RestAssuredTest {
+}
--- BE/src/test/java/com/squadmap/assured/SimpleTest.java
+++ BE/src/test/java/com/squadmap/assured/SimpleTest.java
@@ -3,6 +3,7 @@ package com.squadmap.assured;
 import net.minidev.json.JSONObject;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
