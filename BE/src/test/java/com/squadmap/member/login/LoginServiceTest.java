a01adb8f3b9b80549c7f023b2c25c1ab352fc952
Minseok-Choi <cms05041@gmail.com>
2022-08-30 00:23:53 +0900

    test: HttpClient 사용법 학습

---
+++ BE/src/test/java/com/squadmap/member/login/LoginServiceTest.java
@@ -0,0 +1,64 @@
+package com.squadmap.member.login;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+import static java.net.http.HttpRequest.*;
+import static java.net.http.HttpResponse.*;
+
+class LoginServiceTest {
+
+    @Test
+    void test() throws IOException, InterruptedException {
+
+        URI uri = URI.create("https://github.com/login/oauth/access_token");
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        GithubProperties githubProperties = new GithubProperties("",
+                "",
+                "");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String value = objectMapper.writeValueAsString(githubProperties);
+
+        System.out.println("value = " + value);
+
+        BodyPublisher bodyPublisher = BodyPublishers.ofString(value, StandardCharsets.UTF_8);
+
+        HttpRequest httpRequest = newBuilder().uri(uri)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .POST(bodyPublisher)
+                .build();
+
+        HttpResponse<String> send = httpClient.send(httpRequest, BodyHandlers.ofString());
+        String body = send.body();
+
+        int i = send.statusCode();
+        System.out.println("i = " + i);
+        System.out.println("body = " + body);
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class GithubProperties{
+
+        private String client_id;
+        private String client_secret;
+        private String code;
+
+    }
+}
