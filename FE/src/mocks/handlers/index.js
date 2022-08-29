317c2a93cc027c093e275783553a4df23324366b
Muffin <jinlog9@gmail.com>
2022-08-29 15:00:31 +0900

    refactor: 약간의 스타일링 수정, mock API 추가, 반복문으로 인해 생성된 컴포넌트 key 설정해주기

--- FE/src/mocks/handlers/index.js
+++ FE/src/mocks/handlers/index.js
@@ -10,4 +10,27 @@ const MY_PAGE_MAPS = rest.get('/mypage', (req, res, ctx) =>
   res(ctx.status(200), ctx.delay(1000), ctx.json(myPageMapsData))
 );
 
-export const handlers = [GET_MAPS, MY_PAGE_MAPS];
+const POST_MY_PAGE = rest.post('/mypage', (req, res, ctx) =>
+  res(
+    ctx.status(200),
+    ctx.delay(1000),
+    ctx.json({
+      status: 'success',
+    })
+  )
+);
+
+const PATCH_MY_PAGE = rest.post('/mypage/:patchId', (req, res, ctx) => {
+  const { patchId } = req.params;
+  const patchMypageData = req.body;
+  res(
+    ctx.status(200),
+    ctx.delay(1000),
+    ctx.json({
+      patchId,
+      ...patchMypageData,
+    })
+  );
+});
+
+export const handlers = [GET_MAPS, MY_PAGE_MAPS, POST_MY_PAGE, PATCH_MY_PAGE];
