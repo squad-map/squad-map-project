f7fbf5fd701ffff5c4888cda8439352a4623c5ea
Minseok-Choi <cms05041@gmail.com>
2022-10-27 17:32:51 +0900

    fix: GroupMemberRepository.findMapIdByMemberId jpql 쿼리 추가

--- BE/src/main/java/com/squadmap/group/infrastructure/GroupMemberRepository.java
+++ BE/src/main/java/com/squadmap/group/infrastructure/GroupMemberRepository.java
@@ -2,6 +2,8 @@ package com.squadmap.group.infrastructure;
 
 import com.squadmap.group.domain.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +18,6 @@ public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> 
 
     boolean existsByMapIdAndMemberId(Long mapId, Long memberId);
 
-    List<Long> findMapIdByMemberId(Long memberId);
+    @Query("select gm.id from GroupMember gm where gm.memberId = :memberId")
+    List<Long> findMapIdByMemberId(@Param("memberId") Long memberId);
 }
