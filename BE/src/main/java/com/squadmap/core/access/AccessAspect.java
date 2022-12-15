c50e2b9ee932cbdd5acaed9ef386dd2eb014afa2
Minseok-Choi <cms05041@gmail.com>
2022-12-15 22:28:16 +0900

    commit : redis config 수정 및 AOP를 위한 커스텀 어노테이션 구현

--- BE/src/main/java/com/squadmap/core/access/AccessAspect.java
+++ BE/src/main/java/com/squadmap/core/access/AccessAspect.java
@@ -1,15 +1,39 @@
 package com.squadmap.core.access;
 
 import com.squadmap.core.group.application.GroupMemberService;
-import com.squadmap.core.group.infrastructure.GroupMemberRepository;
+
+import com.squadmap.core.group.domain.PermissionLevel;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+
+@Aspect
 @Component
 @RequiredArgsConstructor
 public class AccessAspect {
 
     private final GroupMemberService groupMemberService;
 
-    
+    @Before("@annotation(com.squadmap.core.access.RequiredPermission)")
+    public void validatePermissionLevel(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        RequiredPermission requiredPermission = signature.getMethod().getAnnotation(RequiredPermission.class);
+        Method method = signature.getMethod();
+        Object[] args = joinPoint.getArgs();
+        PermissionLevel permissionLevel = requiredPermission.requiredLevel();
+        String[] parameterNames = signature.getParameterNames();
+        Class[] parameterTypes = signature.getParameterTypes();
+
+        if (permissionLevel.equals(PermissionLevel.READ)) {
+            //groupMemberRepository.existsByMapIdAndMemberId()
+        }
+    }
+
+
+
 }
