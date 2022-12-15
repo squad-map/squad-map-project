c97bbd2aaa8943dfb0a71d65b2b033e1eafe6cc3
muffin9 <jinlog9@gmail.com>
2022-12-16 00:23:55 +0900

    ♻️ 줄맞춤 재정렬 & 잘못 요청한 API 수정

--- FE/src/components/Authority/GroupInfo/index.tsx
+++ FE/src/components/Authority/GroupInfo/index.tsx
@@ -3,8 +3,12 @@ import { useState } from 'react';
 
 import * as S from './GroupInfo.style';
 
-import { getGroupMembers, putGroupMember, deleteGroupMember } from '@/apis/group';
-import Popup from '@/components/common/Popup'
+import {
+  getGroupMembers,
+  putGroupMember,
+  deleteGroupMember,
+} from '@/apis/group';
+import Popup from '@/components/common/Popup';
 import Text from '@/components/common/Text';
 import theme from '@/styles/theme';
 
@@ -16,7 +20,7 @@ interface GroupInfoRespnse {
 }
 
 const GroupInfo = ({ mapId }: { mapId: number }) => {
-  const [permission, setPermission] = useState("READ");
+  const [permission, setPermission] = useState('READ');
   const [selectedMemberId, setSelectedMemberId] = useState(0);
   const [buttonFlag, setButtonFlag] = useState(0);
 
@@ -30,10 +34,14 @@ const GroupInfo = ({ mapId }: { mapId: number }) => {
     return true;
   });
 
-  const fetchDeleteGroup = useMutation(({ deleteMapId, groupDeleteBody }: {
-    deleteMapId: number;
-    groupDeleteBody: { selectedMemberId: number };
-  }) => deleteGroupMember(deleteMapId, groupDeleteBody),
+  const fetchDeleteGroup = useMutation(
+    ({
+      deleteMapId,
+      groupDeleteBody,
+    }: {
+      deleteMapId: number;
+      groupDeleteBody: { selectedMemberId: number };
+    }) => deleteGroupMember(deleteMapId, groupDeleteBody),
     {
       onSuccess: () => {
         // queryClient.invalidateQueries('GroupInfo');
@@ -41,12 +49,17 @@ const GroupInfo = ({ mapId }: { mapId: number }) => {
       onError: (error: unknown) => {
         throw new Error(`error is ${error}`);
       },
-    });
+    }
+  );
 
-  const fetchPutGroup = useMutation(({ putMapId, groupPutBody }: {
-    putMapId: number;
-    groupPutBody: { member_id: number, permission_level: string };
-  }) => putGroupMember(putMapId, groupPutBody),
+  const fetchPutGroup = useMutation(
+    ({
+      putMapId,
+      groupPutBody,
+    }: {
+      putMapId: number;
+      groupPutBody: { member_id: number; permission_level: string };
+    }) => putGroupMember(putMapId, groupPutBody),
     {
       onSuccess: () => {
         // queryClient.invalidateQueries('GroupInfo');
@@ -54,29 +67,38 @@ const GroupInfo = ({ mapId }: { mapId: number }) => {
       onError: (error: unknown) => {
         throw new Error(`error is ${error}`);
       },
-    });
-
-  console.log(groupMembers);
+    }
+  );
 
   const handleButtonClick = (memberId: number, flag: number) => {
     // flag: 0 : 수정, 1: 삭제
     setButtonFlag(flag);
     setSelectedMemberId(memberId);
     setIsPopup(true);
-  }
+  };
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPermission(e.target.value);
-  }
+  };
 
   const handleSubmitClick = () => {
     if (!selectedMemberId) return;
-    console.log(permission);
-    if (buttonFlag === 0) fetchPutGroup.mutate({ putMapId: mapId, groupPutBody: { member_id: selectedMemberId, permission_level: permission } })
-    else if (buttonFlag === 1) fetchDeleteGroup.mutate({ deleteMapId: mapId, groupDeleteBody: { member_id: selectedMemberId } })
+    if (buttonFlag === 0)
+      fetchPutGroup.mutate({
+        putMapId: mapId,
+        groupPutBody: {
+          member_id: selectedMemberId,
+          permission_level: permission,
+        },
+      });
+    else if (buttonFlag === 1)
+      fetchDeleteGroup.mutate({
+        deleteMapId: mapId,
+        groupDeleteBody: { member_id: selectedMemberId },
+      });
     refetch();
     setIsPopup(false);
-  }
+  };
 
   return (
     <>
@@ -91,22 +113,38 @@ const GroupInfo = ({ mapId }: { mapId: number }) => {
                   size="xSmall"
                   color={theme.color.black}
                 />
-                {member.permission_level === "HOST" ?
+                {member.permission_level === 'HOST' ? (
                   <Text
                     text={member.permission_level}
                     size="xSmall"
                     color={theme.color.black}
                   />
-                  :
+                ) : (
                   <S.SearchSelectBox onChange={handleSelectChange}>
-                    <S.SearchOption value="READ" selected={member.permission_level === "READ"}>READ</S.SearchOption>
-                    <S.SearchOption value="MAINTAIN" selected={member.permission_level === "MAINTAIN"}>MAINTAIN</S.SearchOption>
+                    <S.SearchOption
+                      value="READ"
+                      selected={member.permission_level === 'READ'}
+                    >
+                      READ
+                    </S.SearchOption>
+                    <S.SearchOption
+                      value="MAINTAIN"
+                      selected={member.permission_level === 'MAINTAIN'}
+                    >
+                      MAINTAIN
+                    </S.SearchOption>
                   </S.SearchSelectBox>
-                }
-                <S.Button bgColor={theme.color.navy} onClick={() => handleButtonClick(member.member_id, 0)}>
+                )}
+                <S.Button
+                  bgColor={theme.color.navy}
+                  onClick={() => handleButtonClick(member.member_id, 0)}
+                >
                   <Text text="수정" size="xSmall" color={theme.color.black} />
                 </S.Button>
-                <S.Button bgColor={theme.color.lightRed} onClick={() => handleButtonClick(member.member_id, 1)}>
+                <S.Button
+                  bgColor={theme.color.lightRed}
+                  onClick={() => handleButtonClick(member.member_id, 1)}
+                >
                   <Text text="삭제" size="xSmall" color={theme.color.black} />
                 </S.Button>
               </S.GroupInfo>
@@ -116,7 +154,18 @@ const GroupInfo = ({ mapId }: { mapId: number }) => {
           <div>No Group Data</div>
         )}
       </S.GroupInfoWrapper>
-      {isPopup && <Popup handleSubmitClick={handleSubmitClick} handleCancelClick={() => setIsPopup(false)}><Text text={buttonFlag === 0 ? "수정하시겠습니까?" : "삭제하시겠습니까?"} size="small" color={theme.color.black} /></Popup>}
+      {isPopup && (
+        <Popup
+          handleSubmitClick={handleSubmitClick}
+          handleCancelClick={() => setIsPopup(false)}
+        >
+          <Text
+            text={buttonFlag === 0 ? '수정하시겠습니까?' : '삭제하시겠습니까?'}
+            size="small"
+            color={theme.color.black}
+          />
+        </Popup>
+      )}
     </>
   );
 };
--- FE/src/components/common/Popup/index.tsx
+++ FE/src/components/common/Popup/index.tsx
@@ -1,34 +1,47 @@
 import ReactDom from 'react-dom';
 
 import Button from '../Button';
-import Overlay from "../Overlay"
+import Overlay from '../Overlay';
 
 import * as S from './Popup.style';
 
 import theme from '@/styles/theme';
 
 interface PopupProps {
-    handleSubmitClick: () => void;
-    handleCancelClick: () => void;
-    children: React.ReactNode;
+  handleSubmitClick: () => void;
+  handleCancelClick: () => void;
+  children: React.ReactNode;
 }
 
 const Popup = ({
-    handleSubmitClick,
-    handleCancelClick,
-    children
-}: PopupProps) => ReactDom.createPortal(
+  handleSubmitClick,
+  handleCancelClick,
+  children,
+}: PopupProps) =>
+  ReactDom.createPortal(
     <>
-        <Overlay handleCancelClick={handleCancelClick} />
-        <S.Popup>
-            {children}
-            <S.PopupButtonWrapper>
-                <Button size="xSmall" color={theme.color.navy} onClick={handleSubmitClick}>확인</Button>
-                <Button size="xSmall" color={theme.color.lightRed} onClick={handleCancelClick}>취소</Button>
-            </S.PopupButtonWrapper>
-        </S.Popup>
+      <Overlay handleCancelClick={handleCancelClick} />
+      <S.Popup>
+        {children}
+        <S.PopupButtonWrapper>
+          <Button
+            size="xSmall"
+            color={theme.color.navy}
+            onClick={handleSubmitClick}
+          >
+            확인
+          </Button>
+          <Button
+            size="xSmall"
+            color={theme.color.lightRed}
+            onClick={handleCancelClick}
+          >
+            취소
+          </Button>
+        </S.PopupButtonWrapper>
+      </S.Popup>
     </>,
     document.getElementById('globalModal-root') as HTMLElement
-)
+  );
 
 export default Popup;
