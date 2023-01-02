2b8f2fbe49d7b249097481ef43ba3b5f5039f550
Muffin <jinlog9@gmail.com>
2023-01-02 18:33:14 +0900

    ♻️ 지도 생성, 수정, 삭제시 응답 모달창 구현 완료

--- FE/src/components/Form/index.tsx
+++ FE/src/components/Form/index.tsx
@@ -1,18 +1,20 @@
+import styled from '@emotion/styled/macro';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import * as S from './Form.style';
-
 import { postMypage, patchMypage, deleteMypage } from '@/apis/mypage';
 import Button from '@/components/common/Button';
+import GlobalModal from '@/components/common/GlobalModal';
 import Input from '@/components/common/Input';
 import Text from '@/components/common/Text';
+import ModalContent from '@/components/ModalContent';
 import {
   SUCCESS_POST_MAP,
   SUCCESS_PATCH_MAP,
   SUCCESS_DELETE_MAP,
 } from '@/constants/code';
+import { flexbox } from '@/styles/mixin';
 import theme from '@/styles/theme';
 import { MypagePostParams, MypagePatchParams } from '@/types/mypage';
 import { emojiToUnicode } from '@/utils/util';
@@ -23,8 +25,20 @@ interface FormProps {
   type: boolean;
 }
 
+const Label = styled.label`
+  color: ${theme.color.lightGray};
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const ColumnBox = styled.div`
+  width: 19rem;
+  ${flexbox({ dir: 'column' })}
+  gap: 1rem;
+  margin: 1rem 0;
+`;
+
 const Form = ({ mapId, state, type }: FormProps) => {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -33,12 +47,33 @@ const Form = ({ mapId, state, type }: FormProps) => {
     authority: true,
   });
 
+  const [isModal, setIsModal] = useState(false);
+  const [modalText, setModalText] = useState({
+    title: '',
+    description: '',
+    buttonText: '',
+    handleButtonClick: () => true,
+  });
+
+  const checkFormData = () => {
+    if (formData.map_name === '' || formData.map_emoji === '') return true;
+    return false;
+  };
+
   const fetchPostMypage = useMutation(postMypage, {
     onSuccess: ({ code, data }: { code: string; data: { map_id: number } }) => {
       if (code === SUCCESS_POST_MAP) {
-        queryClient.invalidateQueries('mypageData');
-        // 성공 popup 띄우기.
-        navigate(`/map/${data.map_id}`);
+        setModalText({
+          title: '지도가 생성되었습니다.',
+          description: '지도 생성 성공',
+          buttonText: '확인',
+          handleButtonClick: () => {
+            setIsModal(false);
+            navigate(`/map/${data.map_id}`);
+            return true;
+          },
+        });
+        setIsModal(true);
       }
     },
     onError: (error: unknown) => {
@@ -57,8 +92,17 @@ const Form = ({ mapId, state, type }: FormProps) => {
     {
       onSuccess: ({ code }: { code: string }) => {
         if (code === SUCCESS_PATCH_MAP) {
-          queryClient.invalidateQueries('mypageData');
-          navigate('/mypage');
+          setModalText({
+            title: '지도가 성공적으로 수정되었습니다.',
+            description: '지도 수정 성공',
+            buttonText: '확인',
+            handleButtonClick: () => {
+              setIsModal(false);
+              navigate('/mypage');
+              return true;
+            },
+          });
+          setIsModal(true);
         }
       },
       onError: (error: unknown) => {
@@ -72,8 +116,17 @@ const Form = ({ mapId, state, type }: FormProps) => {
     {
       onSuccess: ({ code }: { code: string }) => {
         if (code === SUCCESS_DELETE_MAP) {
-          queryClient.invalidateQueries('mypageData');
-          navigate('/mypage');
+          setModalText({
+            title: '지도가 성공적으로 삭제되었습니다.',
+            description: '지도 수정 삭제',
+            buttonText: '확인',
+            handleButtonClick: () => {
+              setIsModal(false);
+              navigate('/mypage');
+              return true;
+            },
+          });
+          setIsModal(true);
         }
       },
       onError: (error: unknown) => {
@@ -108,114 +161,144 @@ const Form = ({ mapId, state, type }: FormProps) => {
       full_disclosure: formData.authority,
     };
 
+    if (checkFormData()) {
+      setModalText({
+        title: '지도 이름/이모지를 입력해주세요.',
+        description: '지도 이름 또는 이모지 확인',
+        buttonText: '확인',
+        handleButtonClick: () => {
+          setIsModal(false);
+          return true;
+        },
+      });
+      setIsModal(true);
+      return;
+    }
+
     if (paramId) {
-      if (method === 'patch')
+      if (method === 'patch') {
         fetchPatchMypage.mutate({ paramId, mypageRequestBody: newMypage });
-      else if (method === 'delete') fetchDeleteMypage.mutate(paramId);
+      } else if (method === 'delete') {
+        fetchDeleteMypage.mutate(paramId);
+      }
     } else {
       fetchPostMypage.mutate(newMypage);
     }
   };
 
   return (
-    <S.Form data-testid="map_form">
-      <S.ColumnBox>
-        <S.Label htmlFor="map_name">지도명</S.Label>
-        <Input
-          id="map_name"
-          width="19rem"
-          height="2.5rem"
-          placeholderText="코드스쿼드 주변 맛집"
-          color={theme.color.placeholder}
-          background={theme.color.inputBackground}
-          type="text"
-          value={formData.map_name}
-          onChange={handleMapNameChange}
-        />
-      </S.ColumnBox>
-      <S.ColumnBox>
-        <S.Label htmlFor="map_emoji">이모지</S.Label>
-        <Input
-          id="map_emoji"
-          width="19rem"
-          height="2.5rem"
-          placeholderText="&#x1f6a7; 과 같은 이모지 입력"
-          color={theme.color.placeholder}
-          background={theme.color.inputBackground}
-          type="text"
-          value={formData.map_emoji}
-          onChange={handleEmojiChange}
-        />
-      </S.ColumnBox>
-      <S.ColumnBox>
-        <Text
-          text="지도 공개 설정"
-          size="large"
-          color={theme.color.lightGray}
-        />
-        <div className="flex justify-between px-6">
-          <label htmlFor="public">
-            <input
-              type="radio"
-              name="authority"
-              id="public"
-              value="true"
-              defaultChecked
-              onChange={handleAuthorityChange}
-            />
-            Public
-          </label>
-
-          <label htmlFor="group">
-            <input
-              type="radio"
-              name="authority"
-              id="group"
-              value="false"
-              onChange={handleAuthorityChange}
-            />
-            Group
-          </label>
-        </div>
-      </S.ColumnBox>
-
-      {type ? (
-        <S.ButtonWrapper>
-          <Button
-            type="submit"
-            size="regular"
-            color={theme.color.darkBlue}
-            onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
-              handleSubmit(e, mapId, 'patch')
-            }
-          >
-            <Text text="수정하기" size="regular" color="#fff" />
-          </Button>
-          <Button
-            size="regular"
-            color={theme.color.darkRed}
-            onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
-              handleSubmit(e, mapId, 'delete')
-            }
-          >
-            <Text text="삭제하기" size="regular" color="#fff" />
-          </Button>
-        </S.ButtonWrapper>
-      ) : (
-        <S.ButtonWrapper>
-          <Button
-            type="submit"
+    <>
+      <form className="flex flex-col items-center" data-testid="map_form">
+        <ColumnBox>
+          <Label htmlFor="map_name">지도명</Label>
+          <Input
+            id="map_name"
+            width="19rem"
+            height="2.5rem"
+            placeholderText="코드스쿼드 주변 맛집"
+            color={theme.color.placeholder}
+            background={theme.color.inputBackground}
+            type="text"
+            value={formData.map_name}
+            onChange={handleMapNameChange}
+          />
+        </ColumnBox>
+        <ColumnBox>
+          <Label htmlFor="map_emoji">이모지</Label>
+          <Input
+            id="map_emoji"
+            width="19rem"
+            height="2.5rem"
+            placeholderText="&#x1f6a7; 과 같은 이모지 입력"
+            color={theme.color.placeholder}
+            background={theme.color.inputBackground}
+            type="text"
+            value={formData.map_emoji}
+            onChange={handleEmojiChange}
+          />
+        </ColumnBox>
+        <ColumnBox>
+          <Text
+            text="지도 공개 설정"
             size="large"
-            color="#000"
-            onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
-              handleSubmit(e)
-            }
-          >
-            <Text text="생성하기" size="xRegular" color="#fff" />
-          </Button>
-        </S.ButtonWrapper>
+            color={theme.color.lightGray}
+          />
+          <div className="flex justify-between px-6">
+            <label className="flex items-start" htmlFor="public">
+              <input
+                className="mr-2"
+                type="radio"
+                name="authority"
+                id="public"
+                value="true"
+                defaultChecked
+                onChange={handleAuthorityChange}
+              />
+              Public
+            </label>
+
+            <label className="flex items-start" htmlFor="group">
+              <input
+                className="mr-2"
+                type="radio"
+                name="authority"
+                id="group"
+                value="false"
+                onChange={handleAuthorityChange}
+              />
+              Group
+            </label>
+          </div>
+        </ColumnBox>
+
+        {type ? (
+          <div className="flex gap-4 mt-8">
+            <Button
+              type="submit"
+              size="regular"
+              color={theme.color.darkBlue}
+              onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
+                handleSubmit(e, mapId, 'patch')
+              }
+            >
+              <Text text="수정하기" size="regular" color="#fff" />
+            </Button>
+            <Button
+              size="regular"
+              color={theme.color.darkRed}
+              onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
+                handleSubmit(e, mapId, 'delete')
+              }
+            >
+              <Text text="삭제하기" size="regular" color="#fff" />
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button
+              type="submit"
+              size="large"
+              color="#000"
+              onClick={(e: React.SyntheticEvent<HTMLButtonElement>) =>
+                handleSubmit(e)
+              }
+            >
+              <Text text="생성하기" size="xRegular" color="#fff" />
+            </Button>
+          </div>
+        )}
+      </form>
+      {isModal && (
+        <GlobalModal size="xSmall" handleCancelClick={() => setIsModal(false)}>
+          <ModalContent
+            title={modalText.title}
+            description={modalText.description}
+            buttonText={modalText.buttonText}
+            handleButtonClick={modalText.handleButtonClick}
+          />
+        </GlobalModal>
       )}
-    </S.Form>
+    </>
   );
 };
 
