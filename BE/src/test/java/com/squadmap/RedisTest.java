4d389d57f2004f4af95e0986991cee8200556649
Minseok-Choi <cms05041@gmail.com>
2023-01-17 00:58:24 +0900

    test: 배포를 위해서 임시로 redisTest disabled

--- BE/src/test/java/com/squadmap/RedisTest.java
+++ BE/src/test/java/com/squadmap/RedisTest.java
@@ -1,5 +1,6 @@
 package com.squadmap;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Disabled
 class RedisTest {
 
     @Autowired
